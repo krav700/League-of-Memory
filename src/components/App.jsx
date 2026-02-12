81fe82a34399ec57462a2c49ac354a2ea221acff
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-02-13 01:09:41 +0200

    feat: add card matching algorithm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,93 +8,108 @@ async function getChampsJSON() {
   .then(res => res.json())
   .then(data => data.data)
   .then(data => {
-    // console.log(data)
-    // console.log(randomChamp)
-    // console.log(Object.keys(data));
-    // console.log(Object.keys(data)[randomChamp])
     champsNames = Object.keys(data);
   });
-  // console.log(champsNames);
   return champsNames;
 }
 
-function ChampionImage({champName}) {
+let pickedCards = ["", ""];
 
-  // useEffect(() => {
-  //   const timeOut = setInterval(() => {
-  //     getChampsJSON(setChampName);
-  //   }, 3000)
+function ChampionImage({champ}) {
 
-  //   return () => {
-  //     clearInterval(timeOut);
-  //   }
-  // }, [])
-  let backFacing = true;
   return (
-    <div className='card-container'  onClick={(e) => {
-        if (backFacing) {
-          backFacing = false;
-          console.log()
-          e.target.parentElement.classList.add('rotate');
-        } else {
-          backFacing = true;
-          e.target.parentElement.classList.remove('rotate');
-        }
+    <div className={`card-container ${champ.name}`}  onClick={ (e) => {
+      e.target.parentElement.style.pointerEvents = "none"
+      e.target.parentElement.classList.add('rotate');
+      
+      pickedCards[1] = champ.name;
+      if (!pickedCards[0]) {
+        pickedCards[0] = champ.name;
+        e.target.parentElement.style.pointerEvents = "auto"
+        return;
+      } else {
+        if (pickedCards[0] != pickedCards[1]) {
+              const allCards = document.querySelectorAll('.card-container');
+              allCards.forEach(card => {
+                card.style.pointerEvents = "none";
+              });
+              setTimeout(() => {
+                const champCards = document.querySelectorAll(`.${pickedCards[0]}`)
+                champCards.forEach(card => {
+                    card.classList.remove('rotate');
+                });
+                e.target.parentElement.classList.remove('rotate');
+                pickedCards[0] = "";
+                e.target.parentElement.style.pointerEvents = "auto"
+                allCards.forEach(card => {
+                  card.style.pointerEvents = "auto";
+                });
+              }, 1000)
+            } else {
+              const champCards = document.querySelectorAll(`.${pickedCards[0]}`)
+              champCards.forEach(card => {
+                  card.classList.add("correct-match");
+              });
+              e.target.parentElement.classList.add("correct-match");
+              pickedCards[0] = "";
+            }
+          }
 
       }}>
-      <img className='card-back' src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champName}_0.jpg`} />
+      <img className='card-back' src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.name}_0.jpg`} />
       <img className='card' src={cardBack} />
     </div>
   )
 }
 
-function getChampionName(setCardArray) {
+function getChampionNames(setNameArray) {
   getChampsJSON().then(data => {
-        for (let i = 0; i < 10; i++) {
-          const randomChamp = Math.floor(Math.random() * 172);
-          setCardArray(prev => [...prev, data[randomChamp]]);
-        }
+          setNameArray(data);
       });
+  return true;
+}
+
+function fillArrayWithChamps(numOfCards, setCardArray, nameArray) {
+    for (let i = 0; i < numOfCards; i++) {
+      const randomChamp = Math.floor(Math.random() * 172);
+      setCardArray(prev => [...prev, {name: nameArray[randomChamp], lives: 2}]);
+    }
 }
 
 function App() {
-  const [cardArray, setCardArray] = useState([]);
+  const [nameArray, setNameArray] = useState([]);
+  const [cardArray, setCardArray] = useState([{name: "", lives: 2}]);
 
   let firstRun = true;
   useEffect(() => {
     if (firstRun) {
       firstRun = false;
-      getChampionName(setCardArray);
+      getChampionNames(setNameArray);
     }
     return () => {
-      setCardArray([]);
+      setNameArray([]);
     }
   }, []);
 
   useEffect(() => {
-    console.log(cardArray);
-  }, [cardArray])
+    fillArrayWithChamps(5, setCardArray, nameArray);
+
+    return () => {
+      setCardArray([]);
+    }
+  }, [nameArray]);
+
   return (
     <>
       <div className='cards-container'>
+
         {cardArray.map(card => {
-          return <ChampionImage champName={card} key={card}/>  
+          return <ChampionImage champ={card} key={card.name}/>  
         })}
         {cardArray.map(card => {
-          return <ChampionImage champName={card} key={card}/>  
+          return <ChampionImage champ={card} key={card.name}/>  
         })}
       </div>
-      {/* <ChampionImage champName={cardArray[0]}/>
-      <ChampionImage champName={cardArray[1]}/>
-      <ChampionImage champName={cardArray[2]}/>
-      <ChampionImage champName={cardArray[3]}/>
-      <ChampionImage champName={cardArray[4]}/>
-      <br />
-      <ChampionImage champName={cardArray[5]}/>
-      <ChampionImage champName={cardArray[6]}/>
-      <ChampionImage champName={cardArray[7]}/>
-      <ChampionImage champName={cardArray[8]}/>
-      <ChampionImage champName={cardArray[9]}/> */}
     </>
   )
 }
